2f661f9a7d23d6f94c9051b4ed3ff9284cbebe48
HoangND <duchoangghjk@gmail.com>
2024-05-24 06:21:15 +0700

    one

--- onhost/udagram/udagram-frontend/src/environments/environment.ts
+++ onhost/udagram/udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   apiHost:
-    'http://udacityapi-last-env-1.eba-7mrcx2xh.us-east-1.elasticbeanstalk.com/api/v0',
+    'http://udacityapi-last-env.eba-7mrcx2xh.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
