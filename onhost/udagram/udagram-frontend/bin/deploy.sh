dfe492d7af4932ee7c6d8ee8ce0ee09b8aa39d84
HoangND <duchoangghjk@gmail.com>
2024-05-24 16:16:00 +0700

    final

--- onhost/udagram/udagram-frontend/bin/deploy.sh
+++ onhost/udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,5 @@
-aws s3 cp --recursive --acl public-read ./www s3://hoang-udagram/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://hoang-udagram/
+# aws s3 cp --recursive --acl public-read ./www s3://hoang-udagram/
+# aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://hoang-udagram/
+
+aws s3 cp --recursive --acl public-read ./www $AWS_BUCKET
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html $AWS_BUCKET
